97180d2fbb08bbcd7547eeab0f9d87ad9f4f84c8
mrdimchyk <frostysun@gmail.com>
2026-08-14 09:23:40 +0300

    Add Numotamo collector

---
+++ app/collectors/numotamo.py
@@ -0,0 +1,8 @@
+from app.collectors.generic_jsonld import collect_jsonld
+
+BASE_URL = "https://numotamo.com/uk/catalog/ternopil/all-categories"
+SOURCE_NAME = "Numotamo"
+
+
+def collect(timeout: float = 20.0):
+    return collect_jsonld(BASE_URL, timeout=timeout)
