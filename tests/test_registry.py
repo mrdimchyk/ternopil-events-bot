6ae135d0cd675887890c41b2155003569f06ffca
mrdimchyk <frostysun@gmail.com>
2026-08-15 00:25:47 +0300

    Test collector registry contract

--- tests/test_registry.py
+++ tests/test_registry.py
@@ -1,4 +1,4 @@
-from app.collectors.registry import COLLECTORS
+from app.collectors.registry import COLLECTORS, validate_collectors
 
 
 def test_registry_has_six_sources():
@@ -15,3 +15,7 @@ def test_registry_has_six_sources():
 
 def test_registry_collectors_are_callable():
     assert all(callable(collector) for _, _, collector in COLLECTORS)
+
+
+def test_registry_contract_is_valid():
+    validate_collectors()
