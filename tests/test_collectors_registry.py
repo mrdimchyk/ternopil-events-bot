74b9f6753fbc2ca2c709dbb15f0a65772b47f3d6
mrdimchyk <frostysun@gmail.com>
2026-08-24 03:47:57 +0300

    test: replace registry expectation for Concert.ua promotion

--- tests/test_collectors_registry.py
+++
@@ -1,7 +0,0 @@
-from app.collectors.registry import COLLECTORS, OPTIONAL_COLLECTORS, PRODUCTION_COLLECTORS
-
-
-def test_production_registry_contains_only_verified_mvp_collector():
-    assert COLLECTORS == PRODUCTION_COLLECTORS
-    assert [source for source, _, _ in COLLECTORS] == ["KARABAS"]
-    assert OPTIONAL_COLLECTORS
