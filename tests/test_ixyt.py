a4459fdab2bc337630e039ced67d4ae7754f9661
mrdimchyk <frostysun@gmail.com>
2026-08-29 18:47:59 +0300

    Add regression coverage for current iXYt date format

--- tests/test_ixyt.py
+++ tests/test_ixyt.py
@@ -8,13 +8,15 @@ LIVE_PAGE_HTML = '''
   <table>
     <tr>
       <td>Брешемо чисту правду</td>
-      <td><a href="/event/breshuemo-chistu-pravdu">MORE</a></td>
-      <td>07.09.2026, 18:00</td>
+      <td><a href="/event/breshuemo-chistu-pravdu">ДЕТАЛЬНІШЕ</a></td>
+      <td>2026.09.07, 18:00</td>
+      <td><a href="/event/breshuemo-chistu-pravdu">Квитки</a></td>
     </tr>
     <tr>
-      <td>Гра</td>
-      <td><a href="/event/gra">MORE</a></td>
-      <td>18.11.2026, 18:00</td>
+      <td>МАКС КІДРУК</td>
+      <td><a href="/event/maks-kidruk">ДЕТАЛЬНІШЕ</a></td>
+      <td>2026.11.18, 18:00</td>
+      <td><a href="/event/maks-kidruk">Квитки</a></td>
     </tr>
   </table>
 </body></html>
@@ -26,7 +28,7 @@ def test_ixyt_contract_points_to_ternopil_catalog():
     assert ixyt.BASE_URL == "https://ixyt.info/ua/Ukraine/Ternopil"
 
 
-def test_ixyt_parses_observed_more_link_event_rows(monkeypatch):
+def test_ixyt_parses_observed_current_catalog_date_rows(monkeypatch):
     calls = {}
 
     def fake_get(url, **kwargs):
@@ -41,7 +43,11 @@ def test_ixyt_parses_observed_more_link_event_rows(monkeypatch):
     assert result[0].title == "Брешемо чисту правду"
     assert result[0].start_at.isoformat() == "2026-09-07T18:00:00"
     assert result[0].source_url.endswith("/event/breshuemo-chistu-pravdu")
-    assert result[1].title == "Гра"
+    assert result[1].title == "МАКС КІДРУК"
     assert result[1].start_at.isoformat() == "2026-11-18T18:00:00"
     assert calls["url"] == ixyt.BASE_URL
     assert calls["timeout"] == 7.5
+
+
+def test_ixyt_keeps_legacy_day_first_format_supported():
+    assert ixyt._parse_start("07.09.2026, 18:00", __import__("datetime").datetime(2026, 8, 29, 12, 0)) == __import__("datetime").datetime(2026, 9, 7, 18, 0)
