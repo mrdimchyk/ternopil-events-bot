48ff2af8870fbf411b817739767de8742d1ac75e
mrdimchyk <frostysun@gmail.com>
2026-09-16 02:32:51 +0300

    Update theatre.te production registry regression

--- tests/test_collectors_registry.py
+++ tests/test_collectors_registry.py
@@ -21,5 +21,6 @@ def test_production_registry_contains_verified_collectors():
         "Pulse Live",
         "20 хвилин Тернопіль",
         "Тернопільська обласна філармонія",
+        "Тернопільський академічний театр ім. Т. Г. Шевченка",
     ]
     assert OPTIONAL_COLLECTORS
